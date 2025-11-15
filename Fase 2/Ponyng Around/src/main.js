4926f3d1c2fbb3ed2aa457dbebf76d6d2d380785
silviamor1314 <silviamor1314@gmail.com>
2025-11-15 23:40:09 +0100

    Update main.js

--- Fase 2/Ponyng Around/src/main.js
+++ Fase 2/Ponyng Around/src/main.js
@@ -24,7 +24,11 @@ const config = {
     },
     //scene: [MainMenuScene, SettingsScene, CreditsScene, CharacterSelectScene, RaceScene, PauseScene, FinalScene, FinalProductScene]
     // scene: [PauseScene]
+<<<<<<< Updated upstream
     scene: [MainMenuScene, CharacterSelectScene, RaceScene, PauseScene, FinalScene, FinalProductScene, SettingsScene, CreditsScene]
+=======
+    scene: [IntroScene,IntroAnimScene,MainMenuScene, CharacterSelectScene, RaceScene, PauseScene, FinalScene, FinalProductScene]
+>>>>>>> Stashed changes
     // scene: [MainMenuScene, CharacterSelectScene, RaceScene, FinalScene]
     //scene: [IntroScene,IntroAnimScene,MainMenuScene, CharacterSelectScene, RaceScene, FinalScene, FinalProductScene] <- LA BUENA
 };
